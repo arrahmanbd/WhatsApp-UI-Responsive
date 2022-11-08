6db68102a7f723ed62c8b2a63c0d5b525ccc2833
AR Rahman <arrahman.bd@outlook.com>
2022-11-09 00:46:11 +0600

    Responsive Whatsapp UI

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# WhatsApp-UI-Responsive
-Responsive Design Of WhatsAPP UI Clone
+
+# Responsive Whatsapp UI Clone
+
+A completely Responsive Whatsapp UI App- works on Android, iOS, Web & Desktop! Works as WhatsApp App on Smaller Screen Sizes and WhatsApp Web on Bigger Screen Sizes!
+
+
+
+## Screenshot
+![Screenshot](test/ss.jpg)
+
+
+## Installation
+After cloning this repository, migrate to ```whatsapp-flutter-ui``` folder. There, run the following commands:
+```bash
+  flutter pub get
+  flutter run -d chrome --web-renderer html (to see the best output)
+```
+    
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://arrahmanbd.blogspot.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/arrahmanbd)
---
+++ lib/info.dart
@@ -0,0 +1,123 @@
+const info = [
+  {
+    'name': 'Elon Musk',
+    'message': 'Hey, how are you doing?',
+    'time': '3:53 pm',
+    'profilePic':
+        'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+  },
+  {
+    'name': 'AR Rahman',
+    'message': 'Hello, whats up',
+    'time': '2:25 pm',
+    'profilePic':
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    'name': 'Emila',
+    'message': 'Hello, I want to sleep.',
+    'time': '1:03 pm',
+    'profilePic':
+        'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg',
+  },
+  {
+    'name': 'Professor',
+    'message': 'Call me, have some work',
+    'time': '12:06 pm',
+    'profilePic':
+        'https://pbs.twimg.com/profile_images/1419974913260232732/Cy_CUavB.jpg',
+  },
+  {
+    'name': 'Himu',
+    'message': 'You ate food?',
+    'time': '10:00 am',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Mr Bean',
+    'message': 'Yo!!!!! Long time, no see!?',
+    'time': '9:53 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Albert Einstain',
+    'message': 'Am I fat?',
+    'time': '7:25 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Tesla',
+    'message': 'I am from International Olym...',
+    'time': '6:02 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Newton',
+    'message': 'Lets Code!',
+    'time': '4:56 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1619194617062-5a61b9c6a049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Stan Lee',
+    'message': 'Images by Unsplash',
+    'time': '1:00 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+];
+
+const messages = [
+  {"isMe": false, "text": "Hey What is up with you!!", "time": "10:00 am"},
+  {"isMe": true, "text": "im fine,wbu?", "time": "11:00 am"},
+  {"isMe": false, "text": "I am great man!", "time": "11:01 am"},
+  {
+    "isMe": false,
+    "text": "Just messaged cuz I had some work.",
+    "time": "11:01 am"
+  },
+  {"isMe": true, "text": "Obviously, say", "time": "11:03 am"},
+  {
+    "isMe": false,
+    "text": "haha I wanted you to check out my Twitter",
+    "time": "11:04 am"
+  },
+
+  {
+    "isMe": true,
+    "text": "Looks great to me!",
+    "time": "11:15 am",
+  },
+  
+
+  {"isMe": false, "text": "Thanks bro!", "time": "11:17 am"},
+  {
+    "isMe": false,
+    "text": "Want to travel to the Moon?",
+    "time": "11:16 am"
+  },
+  {
+    "isMe": true,
+    "text": "Yes, surely bro!",
+    "time": "11:17 am"
+  },
+  {
+    "isMe": false,
+    "text": "Cool, did you like SpaceX?",
+    "time": "11:18 am",
+  },
+  {
+    "isMe": true,
+    "text": "I loved it?",
+    "time": "11:19 am",
+  },
+  {
+    "isMe": false,
+    "text": "OMG! Woah! Thanks!",
+    "time": "11:20 am",
+  },
+];
